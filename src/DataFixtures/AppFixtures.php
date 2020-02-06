a56afe28f46280fde0a4f78365baf41af12047d4
MENT3 <clement.rms@gmail.com>
2020-02-06 23:06:45 +0100

    Improve: Fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -12,38 +12,61 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
-        
-        $usr = new User();
-        $usr -> setUsername('to');
+        $usr1 = new User();
+        $usr1 -> setUsername('to');
         // $usr -> setRoles('ROLE_USER');
-        $usr -> setEmail('clement.ramos@gmail.com');
-        $usr -> setPassword('$2y$13$CjtAPpz55FQhDJSGgfr7VupE15RBp9NXCqLYzxf18HJf6B9.CvooK');
-        $usr -> setImg('default.png');
+        $usr1 -> setEmail('clement.ramos@gmail.com');
+        $usr1 -> setPassword('$2y$13$CjtAPpz55FQhDJSGgfr7VupE15RBp9NXCqLYzxf18HJf6B9.CvooK');
+        $usr1 -> setImg('default.png');
         
-        $manager -> persist($usr);
+        $manager -> persist($usr1);
 
-        for($i = 0; $i < 2; $i++) {
-                $grp = new Groupe();
-                $grp -> setName('Groupe ' . $i);
-                $grp -> setUserP($usr);
-                $grp -> setImg('default.png');
-                $grp -> setDate(new \DateTime('now'));
+        $usr2 = new User();
+        $usr2 -> setUsername('me');
+        // $usr -> setRoles('ROLE_USER');
+        $usr2 -> setEmail('clement@gmail.com');
+        $usr2 -> setPassword('$2y$13$CjtAPpz55FQhDJSGgfr7VupE15RBp9NXCqLYzxf18HJf6B9.CvooK');
+        $usr2 -> setImg('default.png');
+        
+        $manager -> persist($usr2);
 
-                $manager -> persist($grp);
+        $grp1 = new Groupe();
+        $grp1 -> setName('Les amis');
+        $grp1 -> setUserP($usr1);
+        $grp1 -> setImg('default.png');
+        $grp1 -> setDate(new \DateTime('now'));
+
+        $manager -> persist($grp1);
+
+        $grp2 = new Groupe();
+        $grp2 -> setName('Profs');
+        $grp2 -> setUserP($usr2);
+        $grp2 -> setImg('default.png');
+        $grp2 -> setDate(new \DateTime('now'));
+
+        $manager -> persist($grp2);
 
 
-            for($j = 0; $j < 5; $j++) {
-                $msg = new Message();
-                $msg -> setGroupe($grp);
-                $msg -> setUser($usr);
-                $msg -> setContent("Ceci est le contenu du message numéro " . $i);
-                $msg -> setDate(new \DateTime('now'));
-                $msg -> setState(1);
+        for($j = 0; $j < 5; $j++) {
+            $msg = new Message();
+            $msg -> setGroupe($grp1);
+            $msg -> setUser($usr1);
+            $msg -> setContent("Ceci est le contenu du message numéro " . $j);
+            $msg -> setDate(new \DateTime('now'));
+            $msg -> setState(1);
 
-                $manager -> persist($msg);
-            }
+            $manager -> persist($msg);
+        }
+
+        for($j = 0; $j < 8; $j++) {
+            $msg = new Message();
+            $msg -> setGroupe($grp2);
+            $msg -> setUser($usr2);
+            $msg -> setContent("Ceci est le contenu du message numéro " . $j);
+            $msg -> setDate(new \DateTime('now'));
+            $msg -> setState(1);
+
+            $manager -> persist($msg);
         }
         $manager -> flush();
     }
