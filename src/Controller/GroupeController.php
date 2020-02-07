0d7f63ffed5a2b8828fbb54dd7396e7572d7815f
MENT3 <clement.rms@gmail.com>
2020-02-07 11:09:10 +0100

    feat: Add persit message

--- src/Controller/GroupeController.php
+++ src/Controller/GroupeController.php
@@ -6,28 +6,59 @@ use App\Entity\User;
 use App\Entity\Groupe;
 use App\Entity\Message;
 use App\Form\GroupeType;
+use App\Form\MessageType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class GroupeController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     /**
      * @Route("/groupe/{id}", name="groupes")
      */
-    public function groupe($id = 6) {
-
+    public function groupe(Request $req, int $id) {
         // Get all messages
         $repo = $this -> getDoctrine() -> getRepository(Groupe::class);
-        $messages = $repo -> find($id) -> getMessages();
+        $groupe = $repo -> find($id);
+        $messages = $groupe -> getMessages();
 
         // Get all groupes of connected user
         $userId = $this -> getUser() -> getId();
         $repo = $this -> getDoctrine() -> getRepository(User::class);
         $groupes = $repo -> find($userId) -> getGroupes();
 
+        // Message form
+        $msg = new Message();
+        $msg -> setUser($this -> getUser());
+        $msg -> setDate(new \DateTime('now'));
+        $msg -> setGroupe($groupe);
+        $msg -> setState(1);
+
+        // Create form
+        $formMessage = $this -> createForm(MessageType::class, $msg);
+
+        $formMessage -> handleRequest($req);
+
+        if($formMessage -> isSubmitted() && $formMessage -> isValid()) {
+
+            // Persist on base
+            $this -> entityManager -> persist($msg);
+            $this -> entityManager -> flush();
+
+        }
+
         return $this -> render('groupe/index.html.twig', [
             'groupes' => $groupes,
-            'messages' => $messages
+            'messages' => $messages,
+            'formMessage' => $formMessage -> createView()
         ]);
     }
 
